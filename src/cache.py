fe0767f3b952766640e0ef971d96be5b8f9d6cf2
Aurelie Mistri <amistri@live.fr>
2018-05-08 12:20:04 +0000

    Heroku handle memcache configs

--- src/cache.py
+++ src/cache.py
@@ -1,5 +1,6 @@
 import pylibmc
 import json, time
+from os import environ
 
 class Lock:
     """
@@ -83,4 +84,31 @@ class Client(pylibmc.Client):
             print e
             return None
 
-mc = Client(['127.0.0.1'])
+# https://devcenter.heroku.com/articles/memcachier#python
+if environ.get('MEMCACHIER_SERVERS', ''):
+    servers = environ.get('MEMCACHIER_SERVERS', '').split(',')
+    user    = environ.get('MEMCACHIER_USERNAME', '')
+    passw   = environ.get('MEMCACHIER_PASSWORD', '')
+
+    mc = Client(servers, binary=True, username=user, password=passw, behaviors={
+      # Faster IO
+      "tcp_nodelay": True,
+
+      # Keep connection alive
+      'tcp_keepalive': True,
+
+      # Timeout for set/get requests
+      'connect_timeout': 2000, # ms
+      'send_timeout': 750 * 1000, # us
+      'receive_timeout': 750 * 1000, # us
+      '_poll_timeout': 2000, # ms
+
+      # Better failover
+      'ketama': True,
+      'remove_failed': 1,
+      'retry_timeout': 2,
+      'dead_timeout': 30,
+    })
+
+else:
+    mc = Client(['127.0.0.1'])
