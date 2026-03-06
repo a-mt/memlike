1d898d91ea26c7a9b3f42faf3a5efdeb1712dfa8
a-mt <amistri@live.fr>
2026-03-06 05:15:43 -0400

    Fix user stuck when changing backend

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -22,6 +22,12 @@ class user:
             user = memrise.user(username)
         except HTTPError as e:
             print(e)
+            if (
+                web.ctx.session.get("loggedin", False)
+                and web.ctx.session["loggedin"]["username"] == username
+            ):
+                return web.config.template.prender._403()
+
             return web.config.template.prender._404()
 
         return web.config.template.render.user(user, tab, USER_RANKS)
