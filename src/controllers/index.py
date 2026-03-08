14779d8a0c6f8f291dcfd611f11b7fb08f110b27
a-mt <amistri@live.fr>
2026-03-08 16:41:58 -0400

    WIP My progress

--- src/controllers/index.py
+++ src/controllers/index.py
@@ -57,21 +57,20 @@ class progress:
                 return (weekday + 1) % 7
             return weekday
 
-        # Beginning and end date
+        # Beginning (first day, either monday or sunday, of first week to show) and end date (today)
         end_date = datetime.date.today()
-        # end_date += relativedelta(days=6 - day_of_week(end_date)) # sunday of last week (0: monday, 6: sunday)
 
         start_date = end_date - relativedelta(months=12)
-        start_date -= relativedelta(days=day_of_week(start_date)) # monday of first week
+        start_date -= relativedelta(days=day_of_week(start_date))
 
         # Get the number of things learned each day
         progress = memrise.my_progress_summary(
             sync_token=int(datetime.datetime.combine(start_date, datetime.datetime.min.time()).timestamp())-1
         )
 
-        # Build the listing of weeks to show
+        # Labels of days, in the order we should display then
+        # ie [{'label': 'Sunday', 'label_short': 'Sun'}...]
         date_increment = relativedelta(days=1)
-
         date = start_date
         days_of_week = []
 
@@ -82,18 +81,18 @@ class progress:
             })
             date += date_increment
 
+        # Build the listing of weeks to show, with the associated progress
+        # ie [{...5: ('2026-03-06', 47), 6: ('2026-03-07', 0)}, {0: ('2026-03-08', 0)}]
         date = start_date
         months = OrderedDict({})
         weeks = []
 
-        min_value = None
-        max_value = None
+        stem_and_leaf = {}
+        bin_size = 10
+        total = 0
 
         while date <= end_date:
-            week = {
-                "days": {},
-            }
-            # '2026-03': {'01': 47, '02': 30, '03': 47, '04': 92, '05': 35}
+            week = {}
 
             # Add days within that week and the progress for these days
             for i in range(0,7):
@@ -103,21 +102,27 @@ class progress:
                 day = date.strftime("%d")
 
                 count = progress.get(month, {}).get(day, 0)
-                week["days"][i] = (date_fmt, count)
+                week[i] = (date_fmt, count)
 
-                # Keep min and max value of progress within start and end date
-                if min_value is None or count < min_value:
-                    min_value = count
+                # Keep track of our counts in a stem&leaf chart
+                # ie {5: [1], 0: [6, 3, 1, 1, 4], 2: [2], 11: [6], 4: [7, 7], 9: [2]}
+                if count:
+                    stem = int(count / bin_size)
+                    leaf = count % bin_size
 
-                if max_value is None or count > max_value:
-                    max_value = count
+                    if stem not in stem_and_leaf:
+                        stem_and_leaf[stem] = []
+                    stem_and_leaf[stem].append(leaf)
+                    total += 1
 
                 date += date_increment
                 if date > end_date:
                     break
+
             weeks.append(week)
 
             # Build month headers
+            # ie {'2025-03': {'count_weeks': 4, 'label': 'March', 'label_short': 'Mar'}}
             if month not in months:
                 months[month] = {
                     "count_weeks": 0,
@@ -126,10 +131,42 @@ class progress:
                 }
             months[month]["count_weeks"] += 1
 
-        # Decide at which threshold we show data-level="0"...data-level="4"
-        c = floor((max_value - min_value) / 4) if min_value != max_value else 1
-        if c < 1:
-            c = 1
+        # Build groups (deciding at which threshold we show data-level="0"...data-level="4")
+        # so that there's roughly the same amount of counts in each level
+        thresholds = [1]
+        if total:
+            stem_and_leaf = {
+                stem: sorted(stem_and_leaf[stem])
+                for stem in sorted(stem_and_leaf.keys())
+            }
+
+            n_groups = 4
+            group_size = int(total / n_groups) if total else 1
+            c = 0
+
+            stems = list(stem_and_leaf.keys())
+            while len(stems):
+                stem = stems.pop(0)
+                n_leaves = len(stem_and_leaf[stem])
+
+                if c + n_leaves < group_size:
+                    c += n_leaves
+                    continue
+
+                leaves = stem_and_leaf[stem]
+                k = group_size - c
+                thresholds.append(int(f"{stem}{leaves[k-1]}"))
+
+                if len(thresholds) == n_groups:
+                    break
+
+                stem_and_leaf[stem] = leaves[k:]
+                stems.insert(0, stem)
+                c = 0
+
+            # ie [(4, 47), (3, 30), (2, 4), (1, 1)]
+            thresholds = [(i+1, t) for i, t in enumerate(thresholds)]
+            thresholds.reverse()
 
         def get_level(thresholds):
             def f(value):
@@ -138,14 +175,10 @@ class progress:
 
                     if value >= threshold:
                         return level
-                if value >= 1:
-                    return 1
                 return 0
             return f
 
-        fn = get_level(thresholds=list(reversed([
-            (i, c*i) for i in range(2,5)
-        ])))
+        fn = get_level(thresholds=thresholds)
 
         return web.config.template.render.progress_heatmap(months, weeks, days_of_week, fn)
 
