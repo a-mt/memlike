e7785d2b2ca60e2759e559d76c413494b07456fa
a-mt <amistri@live.fr>
2026-04-17 08:05:06 -0400

    Build assets

--- static/js/spreadsheet.js
+++ static/js/spreadsheet.js
@@ -1,4 +1,4 @@
-function _asyncToGenerator(fn){return function(){var gen=fn.apply(this,arguments);return new Promise(function(resolve,reject){function step(key,arg){try{var info=gen[key](arg);var value=info.value}catch(error){reject(error);return}if(info.done){resolve(value)}else{return Promise.resolve(value).then(function(value){step('next',value)},function(err){step('throw',err)})}}return step('next')})}}var process=process||{};process.env=process.env||{};window.MEMLIKE=window.MEMLIKE||{};window.MEMLIKE.js_spreadsheet={build_date:'2026-04-16T12:19:45+00:00'};/* global $, window, document, console *//* global fetch */$(document).ready(function(){bindSpreadsheetEvents()});function bindSpreadsheetEvents(){var linkCourse=window.MEMLIKE.course.url,idCourse=window.MEMLIKE.course.id;// Choose to export either multimedia or classic levels
+function _asyncToGenerator(fn){return function(){var gen=fn.apply(this,arguments);return new Promise(function(resolve,reject){function step(key,arg){try{var info=gen[key](arg);var value=info.value}catch(error){reject(error);return}if(info.done){resolve(value)}else{return Promise.resolve(value).then(function(value){step('next',value)},function(err){step('throw',err)})}}return step('next')})}}var process=process||{};process.env=process.env||{};window.MEMLIKE=window.MEMLIKE||{};window.MEMLIKE.js_spreadsheet={build_date:'2026-04-17T12:04:02+00:00'};/* global $, window, document, console *//* global fetch */$(document).ready(function(){bindSpreadsheetEvents()});function bindSpreadsheetEvents(){var linkCourse=window.MEMLIKE.course.url,idCourse=window.MEMLIKE.course.id;// Choose to export either multimedia or classic levels
 var exportElem=document.getElementById('chooseExport0');exportElem&&exportElem.addEventListener('click',chooseExport);exportElem=document.getElementById('chooseExport1');exportElem&&exportElem.addEventListener('click',chooseExport);function chooseExport(){var val=this.value;document.getElementById(`export${val}`).disabled=false;document.getElementById(`export${1-val}`).disabled=true;document.getElementById(`exportAlt`).disabled=val==1;document.getElementById(`exportMore`).disabled=val==1}// Export using in memory data
 document.getElementById('exportInMemory').addEventListener('click',function(){new ExportInMemory});// On render/export
 document.getElementById('spreadsheet_conf').addEventListener('submit',function(e){e.preventDefault();// Get the list of levels selected
@@ -21,7 +21,7 @@ class SpreadSheet{// DOMElement this.body
    * @param boolean exportMore - Export extra columns (visible_info, hidden_info, attributes)
    */constructor(urlCourse,idCourse,levels,exportAlt,exportMore){this.urlCourse=urlCourse;this.idCourse=idCourse;this.levels=levels;this.exportAlt=exportAlt;this.exportMore=exportMore;this.cookies=window.getCookies();// Display a loader
 var container=document.getElementById('spreadsheet'),loading=this.createLoader(container);// Create the spreadsheet
-this.extraHeaders={};this.createBody(container);this.createContent(loading)}/**
+this.extraHeaders={};this.createBody(container);this.createContent(loading);this.bindEvents(container);this.sort={idx:1,asc:1}}bindEvents(container){$('.sort',container).on('click',this.sortColumn.bind(this))}sortColumn(e){var btn=e.target;var th=btn.closest('th');var tbody=th.closest('table').querySelector('tbody');var idx=th.cellIndex;var isNumeric=th.dataset.sort=='numeric';var useDataValue=th.dataset.value=='1';if(idx==this.sort.idx){this.sort.asc=1-this.sort.asc}else{this.sort={idx,asc:1}};var sortDesc=!this.sort.asc;var sortValues=function(tr_a,tr_b){var td_a=tr_a.children[idx],td_b=tr_b.children[idx];var v_a=useDataValue?td_a.dataset.value:td_a.innerText.trim(),v_b=useDataValue?td_b.dataset.value:td_b.innerText.trim();if(sortDesc){[v_b,v_a]=[v_a,v_b]}if(isNumeric){return parseInt(v_a)-parseInt(v_b)}return v_a.localeCompare(v_b)};Array.from(tbody.children).sort(sortValues).forEach(tr=>{tbody.appendChild(tr)})}/**
    * Create the loader and it to the container
    * @param DOMElement container
    * @return DOMElement
@@ -29,12 +29,26 @@ this.extraHeaders={};this.createBody(container);this.createContent(loading)}/**
    * Create a table
    * @return DOMElement
    */createBody(container){var table=document.createElement('table');container.appendChild(table);table.innerHTML=`<thead><tr>
-      <th class="lvl-idx num">${window.I18N['export_column_level']}</th>
-      <th class="item-idx num">#</th>
-      <th class="item-label">${window.I18N['export_column_label']}</th>
-      <th class="item-definition">${window.I18N['export_column_definition']}</th>
-      <th class="score num" colspan="2">${window.I18N['export_column_score']}</th>
-      ${this.exportMore?`<th class="item-more">${window.I18N['export_column_more']}</th>`:``}
+      <th class="lvl-idx num" data-key="export_column_level">
+        ${window.I18N['export_column_level']}
+      </th>
+      <th class="item-idx num" data-value="1" data-key="export_column_index">
+        ${window.I18N['export_column_index']}
+        <button class="sort" type="button">⬍</button>
+      </th>
+      <th class="item-label" data-key="export_column_label">
+        ${window.I18N['export_column_label']}
+        <button class="sort" type="button">⬍</button>
+      </th>
+      <th class="item-definition" data-key="export_column_definition">
+        ${window.I18N['export_column_definition']}
+        <button class="sort" type="button">⬍</button>
+      </th>
+      <th class="score num" colspan="2" data-sort="numeric" data-value="1" data-key="export_column_score">
+        ${window.I18N['export_column_score']}
+        <button class="sort" type="button">⬍</button>
+      </th>
+      ${this.exportMore?`<th class="item-more" data-key="export_column_more">${window.I18N['export_column_more']}</th>`:``}
       </tr></thead>
       <tbody></tbody>`;this.body=table.lastElementChild}getUrl(idLevel){return`/ajax${this.urlCourse}${idLevel}/preview`}/**
    * Populate the body
@@ -48,7 +62,7 @@ scores&&scores[j])}}if(i==n&&loading){loading.parentNode.removeChild(loading);lo
    * @param object data   - Row data
    * @param object score  - User score for current word
    * @param object data
-   */createRow(level,j,data,score){var tr=document.createElement('tr'),html='';html=`<td class="lvl-idx num"><a href="${level.href}">${level.idx}</a></td>`;html+=`<td class="item-idx num">${j+1}</td>`;html+=`<td class="item-label">${this.getValue(data.item)}</td>`;html+=`<td class="item-definition">${this.getValue(data.definition)}</td>`;html+=this.getScore(score);if(this.exportMore){html+=`<td class="item-more">`;if(data.audio){html+=[data.audio].map(it=>{this.addExtraHeader(it.label);return`<div class="more"><span class="highlight">${it.label}</span> ${this.getValue(it,false)}</div>`}).join('')}html+=data.visible_info.map(it=>{this.addExtraHeader(it.label);return`<div class="more"><span class="highlight">${it.label}</span> ${this.getValue(it,false)}</div>`}).join('');html+=data.hidden_info.map(it=>{this.addExtraHeader(it.label);return`<div class="more"><span class="highlight">${it.label}</span> ${this.getValue(it,false)}</div>`}).join('');html+=data.attributes.map(it=>{this.addExtraHeader(it.label);return`<div class="more"><span class="highlight">${it.label}</span> <span>${escapeHTML(it.value)}</span></div>`}).join('');html+=`</td>`}tr.innerHTML=html;this.body.appendChild(tr)}/**
+   */createRow(level,j,data,score){var tr=document.createElement('tr'),html='';html=`<td class="lvl-idx num"><a href="${level.href}">${level.idx}</a></td>`;html+=`<td class="item-idx num" data-value=${level.idx.padStart(4,'0')+'-'+j.toString().padStart(4,'0')}>${j+1}</td>`;html+=`<td class="item-label">${this.getValue(data.item)}</td>`;html+=`<td class="item-definition">${this.getValue(data.definition)}</td>`;html+=this.getScore(score);if(this.exportMore){html+=`<td class="item-more">`;if(data.audio){html+=[data.audio].map(it=>{this.addExtraHeader(it.label);return`<div class="more"><span class="highlight">${it.label}</span> ${this.getValue(it,false)}</div>`}).join('')}html+=data.visible_info.map(it=>{this.addExtraHeader(it.label);return`<div class="more"><span class="highlight">${it.label}</span> ${this.getValue(it,false)}</div>`}).join('');html+=data.hidden_info.map(it=>{this.addExtraHeader(it.label);return`<div class="more"><span class="highlight">${it.label}</span> ${this.getValue(it,false)}</div>`}).join('');html+=data.attributes.map(it=>{this.addExtraHeader(it.label);return`<div class="more"><span class="highlight">${it.label}</span> <span>${escapeHTML(it.value)}</span></div>`}).join('');html+=`</td>`}tr.innerHTML=html;this.body.appendChild(tr)}/**
    * Keep in mind the extra columns in "More"
    * To be able to export the rendered table to CSV
    * @param string label
@@ -61,8 +75,8 @@ scores&&scores[j])}}if(i==n&&loading){loading.parentNode.removeChild(loading);lo
    * Returns HTML: the user's score (correct/attemps)
    * @param object score
    * @return string
-   */getScore(score){if(!score||!score.attempts){return'<td class="score num" colspan="2">-</td>'}var successRate,className;if(score.ignored){successRate='Ignored';className='ignored'}else{successRate=parseInt(score.correct/score.attempts*100)+'%';className=successRate==100?'never-missed':successRate<20?'often-missed':successRate>80?'rarely-missed':'sometimes-missed'}return`<td class="score left num ${className}" title="${successRate}">${this.truncateNum(''+score.correct)}</td>
-            <td class="score right num ${className}" title="${successRate}">${this.truncateNum(''+score.attempts)}</td>`}/**
+   */getScore(score){if(!score||!score.attempts){return'<td class="score num" colspan="2" data-value="0">-</td>'}var successRate,className;if(score.ignored){successRate='Ignored';className='ignored'}else{successRate=parseInt(score.correct/score.attempts*100)+'%';className=successRate==100?'never-missed':successRate<20?'often-missed':successRate>80?'rarely-missed':'sometimes-missed'}return`<td class="score left num ${className}" title="${successRate}" data-value="${successRate}">${this.truncateNum(''+score.correct)}</td>
+            <td class="score right num ${className}" title="${successRate}" data-value="${successRate}">${this.truncateNum(''+score.attempts)}</td>`}/**
    * Make sure the number isn't longer than length, or truncate the left (1012, 3 => 12)
    * @param string str
    * @param integer[optional] length - [3]
@@ -164,7 +178,7 @@ for(let i=0;i<arr.length;i++){this.body+=',';this.body+=arr[i]||''}}this.body+='
    * Retrieve all headers
    * Includes visible_info / hidden_info / attributes if that option was checked
    * @return string
-   */getHeaders(){var headers=[window.I18N['export_column_level'],'#',window.I18N['export_column_label'],window.I18N['export_column_definition'],window.I18N['export_column_score_correct'],window.I18N['export_column_score_attempts'],window.I18N['export_column_score_percent']].join(',');if(!this.exportMore){return headers}var extra=[];for(var label in this.headers){extra[this.headers[label]]=escapeCSV(label)}return headers+(extra.length?','+extra.join(','):'')}}//+--------------------------------------------------------
+   */getHeaders(){var headers=[window.I18N['export_column_level'],window.I18N['export_column_index'],window.I18N['export_column_label'],window.I18N['export_column_definition'],window.I18N['export_column_score_correct'],window.I18N['export_column_score_attempts'],window.I18N['export_column_score_percent']].join(',');if(!this.exportMore){return headers}var extra=[];for(var label in this.headers){extra[this.headers[label]]=escapeCSV(label)}return headers+(extra.length?','+extra.join(','):'')}}//+--------------------------------------------------------
 //|
 //| EXPORT CSV - MULTIMEDIA
 //|
@@ -189,11 +203,11 @@ class ExportMultimedia extends SpreadSheetMultimedia{/**
 //+--------------------------------------------------------
 class ExportInMemory{/**
    * Entrypoint
-   */constructor(){var container=document.getElementById('spreadsheet'),body=container.querySelector('tbody'),filename=body.dataset.file;var extraHeaders=this.decodeExtraHeaders(body.dataset.extraHeaders),headers=Array.from(container.querySelector('thead tr').children).map(node=>node.innerText);var csv=this.getHeaders(headers,extraHeaders)+'\n'+this.getData(body,headers,extraHeaders);window.download(csv,filename||'Memrise-'+this.idCourse+'.csv','text/csv')}/**
+   */constructor(){var container=document.getElementById('spreadsheet'),body=container.querySelector('tbody'),filename=body.dataset.file;var extraHeaders=this.decodeExtraHeaders(body.dataset.extraHeaders),headers=Array.from(container.querySelector('thead tr').children).map(node=>node.dataset.key);var csv=this.getHeaders(headers,extraHeaders)+'\n';csv+=this.getData(body,headers,extraHeaders);window.download(csv,filename||'Memrise-'+this.idCourse+'.csv','text/csv')}/**
    * @param array headers
    * @param array extraHeaders
    * @return string
-   */getHeaders(_headers,extraHeaders){var headers=[..._headers];var k=headers.indexOf(window.I18N['export_column_score']);if(k!=-1){headers.splice(k,1,...[window.I18N['export_column_score_correct'],window.I18N['export_column_score_attempts'],window.I18N['export_column_score_percent']])}k=headers.indexOf(window.I18N['export_column_more']);if(k!=-1){headers.splice(k,1,...extraHeaders)}k=headers.indexOf(window.I18N['export_column_content']);if(k!=-1){headers.splice(k,1,...[window.I18N['export_column_label'],window.I18N['export_column_content']])}return headers.map(label=>escapeCSV(label)).join(',')}/**
+   */getHeaders(_headers,extraHeaders){var headers=[..._headers];var k=headers.indexOf('export_column_score');if(k!=-1){headers.splice(k,1,...['export_column_score_correct','export_column_score_attempts','export_column_score_percent'])}k=headers.indexOf('export_column_more');if(k!=-1){headers.splice(k,1,...extraHeaders)}k=headers.indexOf('export_column_content');if(k!=-1){headers.splice(k,1,...['export_column_label','export_column_content'])}return headers.map(label=>escapeCSV(window.I18N[label])).join(',')}/**
    * Retrieve the JSON-decoded list of extra headers
    * Or an empty list
    *
@@ -203,10 +217,10 @@ class ExportInMemory{/**
    * Retrieve the rendered table as a CSV string
    * @param DOMElement body
    * @return string
-   */getData(body,headers,extraHeaders){var csv='';for(let i=0;i<body.children.length;i++){let tr=body.children[i],data=[];let k=0;for(let j=0;j<headers.length;j++){let label=headers[j],td=tr.children[k];switch(label){case window.I18N['export_column_level']:case'#':csv+=td.innerText+',';break;case window.I18N['export_column_score']:if(!td.hasAttribute('colspan')){let correct=parseInt(td.innerText,10),attempt=parseInt(tr.children[k+1].innerText,10);k++;csv+=correct+',';csv+=attempt+',';csv+=parseInt(correct/attempt*100)}else{csv+=',,'}break;case window.I18N['export_column_more']:let more=td.querySelectorAll('.more'),extra={};// Retrieve all additionnal that have been defined
+   */getData(body,headers,extraHeaders){var csv='';for(let i=0;i<body.children.length;i++){let tr=body.children[i],data=[];let k=0;for(let j=0;j<headers.length;j++){let slug=headers[j],td=tr.children[k];switch(slug){case'export_column_level':case'export_column_index':csv+=td.innerText+',';break;case'export_column_score':if(!td.hasAttribute('colspan')){let correct=parseInt(td.innerText,10),attempt=parseInt(tr.children[k+1].innerText,10);k++;csv+=correct+',';csv+=attempt+',';csv+=parseInt(correct/attempt*100)}else{csv+=',,'}break;case'export_column_more':let more=td.querySelectorAll('.more'),extra={};// Retrieve all additionnal that have been defined
 for(let j2=0;j2<more.length;j2++){let label=more[j2].firstElementChild.innerText,content=this.getValue(more[j2].lastElementChild);extra[label]=escapeCSV(content)}// Put them in order
 for(let j2=0;j2<extraHeaders.length;j2++){let label=extraHeaders[j2];csv+=','+(typeof extra[label]=='undefined'?'':extra[label])}break;// Multimedia
-case window.I18N['export_column_content']:csv+=escapeCSV(td.children[0].innerText.trim())+',';csv+=escapeCSV(td.children[1].innerText.trim());break;default:csv+=escapeCSV(this.getValue(td.firstElementChild,true))+',';break;}k++}csv+='\n'}return csv}/**
+case'export_column_content':csv+=escapeCSV(td.children[0].innerText.trim())+',';csv+=escapeCSV(td.children[1].innerText.trim());break;default:csv+=escapeCSV(this.getValue(td.firstElementChild,true))+',';break;}k++}csv+='\n'}return csv}/**
    * Retrieve the text of a DOMElement
    * @param DOMElement node
    * @param boolean siblings - [false] Return the content of siblings too
